9a277c9fd10357a245b6963c108ed753174e7afb
lmodin <lisamodin@live.com>
2020-08-19 11:17:26 -0700

    Refactored display to a grid for the projects list on left side and map on the right

---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+let ProjectsContainer = styled.div`
+  display: grid;
+  grid-column: 1;
+  grid-row: 1;
+`
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+    <div>
+      Projects list will go here
+    </div>
+    )
+  }
+}
+
+export default Projects;
