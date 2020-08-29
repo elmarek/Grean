0952c031892ce1a2681593b14c3b5982683a5e8b
lmodin <lisamodin@live.com>
2020-08-29 13:11:46 -0700

    Updated the quality of the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo1 from '../../Grean0.png';
+import Logo1 from '../../Grean1.png';
 
 let StyledHeader = styled.div`
   height: 80px;
