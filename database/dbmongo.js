4adcf4a30565800f9675f32b47996046674d28c5
lmodin <lisamodin@live.com>
2020-09-03 13:24:05 -0700

    Updated routes in server for get projects, events, comments. wrote naive  queries in dbmongo for reads, creates, updates

--- database/dbmongo.js
+++ database/dbmongo.js
@@ -6,11 +6,23 @@ mongoose.connect('mongodb://localhost:27017/grean', {
   useUnifiedTopology: true
 })
 
+/*
+=========================================================
+Mongoose connection
+=========================================================
+*/
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '))
 db.once('open', function() {
   console.log('Connected to mongoose!')
 })
+
+/*
+=========================================================
+Project schema and data types
+=========================================================
+*/
 
 let projectSchema = new mongoose.Schema({
   name: String,
@@ -34,6 +46,13 @@ let projectSchema = new mongoose.Schema({
   litterTypes: [ String ],
   projectType: [ String ],
 })
+let Project = mongoose.model("Project", projectSchema)
+
+/*
+=========================================================
+Event schema and data types
+=========================================================
+*/
 
 let eventSchema = new mongoose.Schema({
   project: String,
@@ -42,7 +61,7 @@ let eventSchema = new mongoose.Schema({
   created_by: String,
   created_at: {
     type: Date,
-    default: Date.now()
+    default: Date.now
   },
   date_start: Date,
   date_end: Date,
@@ -54,6 +73,13 @@ let eventSchema = new mongoose.Schema({
   attendees: [ String ],
   equipment: [ String ]
 })
+let Event = mongoose.model('Event', eventSchema)
+
+/*
+=========================================================
+Users schema and data types
+=========================================================
+*/
 
 let userSchema = new mongoose.Schema({
   username: {
@@ -73,16 +99,120 @@ let userSchema = new mongoose.Schema({
   saved_projects: [ String ],
   events_attended: [ String ]
 })
+let User = mongoose.model('User', userSchema)
+
+/*
+=========================================================
+Comments schema and data types
+=========================================================
+*/
 
 let commentSchema = new mongoose.Schema({
   event: String,
   project: String,
   user: String,
-  comment: String
+  comment: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 })
-
-let Project = mongoose.model("Project", projectSchema)
-let Event = mongoose.model('Event', eventSchema)
-let User = mongoose.model('User', userSchema)
 let Comment = mongoose.model('Comment', commentSchema)
 
+/*
+=========================================================
+Read query routes
+=========================================================
+*/
+
+const getAllProjects = () => {
+  //get all projects
+  //take location as an input to query
+  //find based on math of map shown in window
+  var projects = Project.find({})
+
+  return projects;
+}
+
+const getAllEvents = () => {
+  //get all events
+  //take location as an input to query
+  //find based on math of map shown in window
+  //filter upcoming only by date_start
+  var events = Event.find({})
+
+  return events;
+}
+
+const getEventsByProject = (project) => {
+  //get events by project id
+  var events = Event.find({ project: project }).lean()
+  //filter by upcoming
+  //sort by start_date (soonest first)
+  return events;
+}
+
+const getComments = (project, event) => {
+  //get comments associated with a specific project or event (or both)
+  var comments = [];
+  if (!event) {
+    comments = Comment.find({ project: project })
+  } else {
+    comments = Comment.find({ event: event })
+  }
+  //sort by most recent based on created_at
+
+  return comments;
+}
+
+/*
+=========================================================
+Create query routes
+=========================================================
+*/
+
+const saveProject = (project) => {
+
+}
+const saveEvent = (event) => {
+
+}
+const saveUser = (user) => {
+
+}
+const saveComment = (comment) => {
+
+}
+
+/*
+=========================================================
+Update query routes
+=========================================================
+*/
+const newRSVP = (rsvp) => {
+  let event = Event.findByIdAndUpdate(rsvp.event, {
+    // taken from MVP, adds attendee to array
+    $push: {attendees: rsvp.name},
+  });
+  return event;
+}
+
+/*
+=========================================================
+Delete query routes
+=========================================================
+*/
+
+
+/*
+=========================================================
+Exports
+=========================================================
+*/
+
+module.exports = {
+  getAllProjects: getAllProjects,
+  getAllEvents: getAllEvents,
+  getEventsByProject: getEventsByProject,
+  getComments: getComments
+}
